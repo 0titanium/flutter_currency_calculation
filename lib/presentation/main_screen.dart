d6059af21065b368e7f05c454eeefaa8b9e67dcc
gyubit <gyubros@gmail.com>
2024-04-16 14:30:20 +0900

    UPDATE

--- lib/presentation/main_screen.dart
+++ lib/presentation/main_screen.dart
@@ -5,19 +5,38 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _textEdtingController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Currency'),
       ),
       body: Container(
-        child: SizedBox(
-          width:  150,
-          child: DropdownButton(
-            items: const [
-              DropdownMenuItem(child: Text('USD'), value: 'USD'),
-              DropdownMenuItem(child: Text('KRW'), value: 'KRW'),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 200,
+                    height: 40,
+                    child: TextField(
+                      controller: _textEdtingController,
+                        decoration: InputDecoration(
+                          hintText: '숫자를 입력하세요'
+                        ),
+                    ),
+                  ),
+                  DropdownButton(
+                    items: const [
+                      DropdownMenuItem(child: Text('USD'), value: 'USD'),
+                      DropdownMenuItem(child: Text('KRW'), value: 'KRW'),
+                    ],
+                    onChanged: (String? value) {},
+                  ),
+                ],
+              ),
             ],
-            onChanged: (String? value) {},
           ),
         ),
       ),
